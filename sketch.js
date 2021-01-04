96a31826efcb304682a44cb97d958bc1333f7bc0
FeNiX-2 <73537163+FeNiX-2@users.noreply.github.com>
2021-01-04 17:11:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,124 @@
+var canvas, bgImg;
+var database;
+
+var form, game ,player;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var car1,car2,car3,car4,car5,cars;                                           
+
+function setup(){
+    canvas = createCanvas(displayWidth-20,displayHeight-30);
+    database = firebase.database();
+game=new Game()
+game.getState()
+game.start()
+
+}
+
+function draw(){
+
+    if(playerCount === 4){
+        game.update(1);
+    }
+
+    if(gameState === 1){
+        clear();
+        game.play();
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
